6331a3189592d728a4b506a3866548f971b5e161
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-03-29 10:39:59 +0200

    Book AggregateRoot in progress

---
+++ Library.Domain/AggregateModels/BookAggregate/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Library.Domain.SeedWork;
+
+namespace Library.Domain.AggregateModels.BookAggregate
+{
+    public class Book : Entity<long>
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public bool IsReserved { get; }
+        public DateTime? ReservedUntil { get; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            IsReserved = false;
+            ReservedUntil = null;
+        }
+    }
+}
