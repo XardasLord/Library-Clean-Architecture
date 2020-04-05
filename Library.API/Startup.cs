170f25ac720f39ac7fb55550bcd4726dbe193a99
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-04-05 14:34:14 +0200

    Registered application layer with MediatR

--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Application;
 using Library.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace Library.API
             services.AddControllers();
 
             services.AddInfrastructure(Configuration);
+            services.AddApplication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
