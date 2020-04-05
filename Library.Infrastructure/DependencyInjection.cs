22e3ec9eb435bff23cd86099d60a71e0b950803f
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-04-05 10:59:36 +0200

    Added Persistance configuration in the infrastructure layer

---
+++ Library.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Library.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+            => AddDatabase(services, configuration);
+
+        private static IServiceCollection AddDatabase(IServiceCollection services, IConfiguration configuration)
+            => services.AddDbContext<LibraryDbContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection"));
+                });
+    }
+}
---
+++ Library.Infrastructure/Persistence/LibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.Persistence
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
