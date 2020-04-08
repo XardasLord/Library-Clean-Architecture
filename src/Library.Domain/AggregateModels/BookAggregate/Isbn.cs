112b2a9295bfd77884b1f97975bb7f39453a5e33
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-04-08 10:31:13 +0200

    Isbn ValueObject

---
+++ src/Library.Domain/AggregateModels/BookAggregate/Isbn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Library.Domain.Exceptions;
+using Library.Domain.SeedWork;
+
+namespace Library.Domain.AggregateModels.BookAggregate
+{
+    public class Isbn : ValueObject
+    {
+        // https://howtodoinjava.com/regex/java-regex-validate-international-standard-book-number-isbns/
+        private static readonly Regex Isbn10FormatPattern = new Regex(@"^(?:ISBN(?:-10)?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$)[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$");
+        private static readonly Regex Isbn13FormatPattern = new Regex(@"^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9]$");
+        
+        public string Value { get; }
+
+        public Isbn(string isbn)
+        {
+            const string nonDigitsPattern = "[^.0-9]";
+            isbn = Regex.Replace(isbn, nonDigitsPattern, string.Empty);
+
+            if (!Isbn10FormatPattern.IsMatch(isbn) && !Isbn13FormatPattern.IsMatch(isbn))
+                throw new BookIsbnInvalidFormatException(isbn);
+
+            Value = isbn;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+
+        public override string ToString() => Value;
+    }
+}
---
+++ src/Library.Domain/Exceptions/BookIsbnInvalidFormatException.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Domain.Exceptions
+{
+    public class BookIsbnInvalidFormatException : DomainException
+    {
+        protected override string Code => "invalid_isbn_format";
+        public string Isbn { get; }
+
+        public BookIsbnInvalidFormatException(string isbn) : base($"ISBN value has to be 10 or 13 length. Passed ISBN number is: {isbn}.") 
+            => Isbn = isbn;
+    }
+}
