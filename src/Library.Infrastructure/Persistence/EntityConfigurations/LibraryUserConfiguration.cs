f9a7175c5c78da458d35f0486b822f300cae2a96
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-04-13 09:11:52 +0200

    DB migration - LibraryUsers

---
+++ src/Library.Infrastructure/Persistence/EntityConfigurations/LibraryUserConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Library.Domain.AggregateModels.LibraryUserAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Infrastructure.Persistence.EntityConfigurations
+{
+    public class LibraryUserConfiguration : IEntityTypeConfiguration<LibraryUser>
+    {
+        public void Configure(EntityTypeBuilder<LibraryUser> entity)
+        {
+            entity.ToTable("LibraryUser");
+            entity.HasKey(x => x.Id);
+
+            entity.Ignore(x => x.DomainEvents);
+
+            entity.Property(x => x.Id)
+                .HasColumnName("LibraryUserId")
+                .UseIdentityColumn();
+
+            entity.Property(x => x.Login)
+                .HasColumnName("Login")
+                .IsRequired();
+
+            entity.Property(x => x.Password)
+                .HasColumnName("Password")
+                .IsRequired();
+
+            entity.Property(x => x.FirstName)
+                .HasColumnName("FirstName")
+                .IsRequired();
+
+            entity.Property(x => x.LastName)
+                .HasColumnName("LastName")
+                .IsRequired();
+
+            entity.Property(x => x.IsActive)
+                .HasColumnName("IsActive");
+
+            entity.OwnsOne(x => x.Email, x =>
+            {
+                x.Property(e => e.Value)
+                    .HasColumnName("Email")
+                    .IsRequired();
+            });
+        }
+    }
+}
--- src/Library.Infrastructure/Persistence/Migrations/20200412171142_AddLibraryUsersTable.Designer.cs
+++ src/Library.Infrastructure/Persistence/Migrations/20200412171142_AddLibraryUsersTable.Designer.cs
@@ -3,14 +3,16 @@ using Library.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Library.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200412171142_AddLibraryUsersTable")]
+    partial class AddLibraryUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
