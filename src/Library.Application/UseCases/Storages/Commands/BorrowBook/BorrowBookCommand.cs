6b644e8968da0179aea25dba9bb673bd055e0e8f
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-04-25 10:16:24 +0200

    Borrowing book command

---
+++ src/Library.Application/UseCases/Storages/Commands/BorrowBook/BorrowBookCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+
+namespace Library.Application.UseCases.Storages.Commands.BorrowBook
+{
+    public class BorrowBookCommand : IRequest
+    {
+        public long BookId { get; set; }
+        public long UserId { get; set; }
+        public DateTime BorrowingEndDate { get; set; }
+    }
+}
