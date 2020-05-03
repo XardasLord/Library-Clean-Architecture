8a414257f4298f72c22d2bae027d97b825954f0b
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-05-03 10:17:32 +0200

    DB Migration

---
+++ src/Library.Infrastructure.Persistence/Migrations/20200426034351_StartDateColumnInLoanTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Infrastructure.Persistence.Migrations
+{
+    public partial class StartDateColumnInLoanTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Loan",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Loan",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Loan");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Loan",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
