1c4865ff71a6a57912925e9e06a717022713ffeb
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-05-05 08:12:39 +0200

    Borrowing book improvement - getting user ID from jwt token, not from command

--- src/Library.Application/UseCases/Storages/Commands/BorrowBook/BorrowBookCommand.cs
+++ src/Library.Application/UseCases/Storages/Commands/BorrowBook/BorrowBookCommand.cs
@@ -6,7 +6,6 @@ namespace Library.Application.UseCases.Storages.Commands.BorrowBook
     public class BorrowBookCommand : IRequest
     {
         public long BookId { get; set; }
-        public long UserId { get; set; }
         public DateTime BorrowingStartDate { get; set; }
         public DateTime BorrowingEndDate { get; set; }
     }
