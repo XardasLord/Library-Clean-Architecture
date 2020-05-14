e151962c69116c4e6c119c44c92e7d143041d780
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-05-14 15:52:48 +0200

    UserCredendials Value object

---
+++ src/Library.Domain/AggregateModels/LibraryUserAggregate/UserCredential.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Security.Authentication;
+using Library.Domain.SeedWork;
+
+namespace Library.Domain.AggregateModels.LibraryUserAggregate
+{
+    public class UserCredential : ValueObject
+    {
+        public string Login { get; }
+        public string Password { get; }
+
+        private UserCredential() { }
+
+        public UserCredential(string login, string password)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+                throw new InvalidCredentialException($"Parameter {nameof(login)} cannot be empty.");
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new InvalidCredentialException($"Parameter {nameof(password)} cannot be empty.");
+
+            if (password.Length <= 5)
+                throw new InvalidCredentialException("Password has to be at least 6 characters length.");
+
+            Login = login;
+            Password = password;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Login;
+            yield return Password;
+        }
+    }
+}
