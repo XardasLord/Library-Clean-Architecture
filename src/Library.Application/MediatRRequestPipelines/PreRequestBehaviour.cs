8257b3e3d47afbabddc1fa95f47d1354120454e5
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-07-08 07:39:29 +0200

    Added logging for each query/command using IRequestPreProcessor & IRequestPostProcessor from MediatR feature

---
+++ src/Library.Application/MediatRRequestPipelines/PreRequestBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Library.Application.MediatRRequestPipelines
+{
+	public class PreRequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public PreRequestLogger(ILogger<TRequest> logger)
+            => _logger = logger;
+
+        public Task Process(TRequest request, CancellationToken cancellationToken = default)
+        {
+            var requestName = typeof(TRequest).Name;
+
+            _logger.LogInformation("Request started: {requestName}", requestName);
+
+            return Task.CompletedTask;
+        }
+    }
+}
